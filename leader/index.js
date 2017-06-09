b5df7acb72137ab7fb4470b314368d47f9d0be28
Michael Williams <michael.williams@enspiral.com>
2017-06-09 20:57:51 +1200

    start to cycle

--- leader/index.js
+++ leader/index.js
@@ -1,10 +1,9 @@
-const React = require('react')
-const h = require('react-hyperscript')
-const { createRenderer } = require('fela')
-const { Provider: FelaProvider } = require('react-fela')
-const PixelsGl = require('pixels-gl')
-const { Surface } = require('gl-react-dom')
+const xs = require('xstream').default
+const { run } = require('@cycle/run')
+const { div, label, input, hr, h1 } = require('@cycle/dom')
+const { makeFelaDomDriver, createComponent } = require('cycle-fela')
 const insertCss = require('insert-css')
+const onionify = require('cycle-onionify').default
 
 insertCss(`
    html, body, .main, canvas {
@@ -14,6 +13,46 @@ insertCss(`
     padding: 0;
   }      
 `)
+
+const Container = createComponent(() => ({}))
+
+function Leader (sources) {
+  const { state$ } = sources.onion
+
+  const vdom$ = state$
+    .map(({ name }) => {
+      return Container([
+        label('Name:'),
+        input('.myinput', {attrs: {type: 'text'}}),
+        hr(),
+        h1(`Hello ${name}`)
+      ])
+    })
+  
+  const initReducer$ = xs.of(() => ({ name: '' }))
+  const updateReducer$ = sources.DOM
+    .select('.myinput').events('input')
+    .map(ev => ev.target.value)
+    .map(name => () => ({ name }))
+  const reducer$ = xs.merge(initReducer$, updateReducer$)
+
+  return {
+    DOM: vdom$,
+    onion: reducer$
+  }
+}
+
+run(onionify(Leader), {
+  DOM: makeFelaDomDriver('#app', {
+    customStyleNode: document.querySelector('#app-styles')
+  })
+})
+/*
+const React = require('react')
+const h = require('react-hyperscript')
+const { Provider: FelaProvider } = require('react-fela')
+const PixelsGl = require('pixels-gl')
+const { Surface } = require('gl-react-dom')
 
 const Resux = require('../lib/resux')
 const SceneList = require('../scenes/components/sceneList')
@@ -58,3 +97,4 @@ Resux(
   modules,
   document.querySelector('#app')
 )
+*/
